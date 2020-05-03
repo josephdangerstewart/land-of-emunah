31f056af12eb95fd06549a23fc71701d441b1a5b
Joseph Stewart <joseph.s@nationalschoolproject.com>
2020-05-02 23:06:56 -0700

    Maybe 100% instead of 100vw?

--- components/card/card-elements.tsx
+++ components/card/card-elements.tsx
@@ -16,8 +16,8 @@ export const Overlay = styled.div<{ zIndex?: number}>`
 	position: absolute;
 	top: 0;
 	left: 0;
-	width: 100vw;
-	height: 100vh;
+	width: 100%;
+	height: 100%;
 	display: flex;
 	justify-content: center;
 	overflow: hidden;
