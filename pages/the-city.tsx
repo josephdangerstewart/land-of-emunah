00a7dc73f0d6e38fbf4bfdc4cf1cfea6c6d94aeb
Joseph Stewart <joseph.s@nationalschoolproject.com>
2020-05-02 17:40:15 -0700

    Fix typo in the city

--- pages/the-city.tsx
+++ pages/the-city.tsx
@@ -22,7 +22,7 @@ export default function TheCity() {
 			<Location
 				title="The City"
 				coverImageUrl="/images/locations/city.png"
-				bodyText="Your journey starts with leaving the city in the center of the land of Emunah. This city is always bustling and busy with many people constantly moving around and continuing through life at their own pace. You have grown to wonder what is further beyond this crowded, nosy city and have decided to set out on a new adventure to explore the Land of Emunah."
+				bodyText="Your journey starts with leaving the city in the center of the land of Emunah. This city is always bustling and busy with many people constantly moving around and continuing through life at their own pace. You have grown to wonder what is further beyond this crowded, noisy city and have decided to set out on a new adventure to explore the Land of Emunah."
 				onContinue={handleOnContinue}
 				encounter={{
 					description: 'As you are heading out of the city gates, ready to embark on a new adventure, you see that there are four different paths in front of you. Which direction do you go?',
