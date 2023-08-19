cc22fa41162818a75d3e00a6550816b289b5d401
Joseph Stewart <josephlikescode@gmail.com>
2023-08-19 16:08:10 -0700

    Use yaml repositories.

--- api/type-registry.ts
+++ api/type-registry.ts
@@ -1,11 +1,10 @@
 import { IEncounterRepository } from '../types/IEncounterRepository';
 import { ILocationRepository } from '../types/ILocationRepository';
-import { DirectusLocationRepository } from './DirectusLocationRepository';
-import { DirectusEncounterRepository } from './DirectusEncounterRepository';
 import { IPromptRepository } from '../types/IPromptRepository';
 import { DirectusPromptRepository } from './DirectusPromptRepository';
+import { YamlEncounterRepository } from './YamlEncounterRepository';
+import { YamlLocationRepository } from './YamlLocationRepository';
 
-const DIRECTUS_IMAGE_URL = 'https://admin.landofemunah.com';
 const DIRECTUS_DEV_URL = 'https://admin.landofemunah.com/admin';
 const DIRECTUS_PROD_URL = 'https://admin.landofemunah.com/admin';
 
@@ -14,12 +13,14 @@ const directusBaseUri = process.env.NODE_ENV === 'development'
 		: DIRECTUS_PROD_URL;
 
 export async function getEncounterRepository(): Promise<IEncounterRepository> {
-	const repository = new DirectusEncounterRepository(directusBaseUri, DIRECTUS_IMAGE_URL);
+	const repository = new YamlEncounterRepository('./encounters');
+	await repository.init();
 	return repository;
 }
 
 export async function getLocationRepository(): Promise<ILocationRepository> {
-	const repository = new DirectusLocationRepository(directusBaseUri, DIRECTUS_IMAGE_URL);
+	const repository = new YamlLocationRepository('./locations');
+	await repository.init();
 	return repository;
 }
 
