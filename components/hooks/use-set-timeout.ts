0adcd5d114169e95e6b4de1529a488cdb316ec37
Joseph Stewart <josephlikescode@gmail.com>
2023-08-23 22:36:20 -0700

    Upgrade typescript

--- components/hooks/use-set-timeout.ts
+++ components/hooks/use-set-timeout.ts
@@ -3,7 +3,7 @@ import { useRef, useCallback, useEffect } from 'react';
 export type SetTimeoutType = (handler: (...args: any[]) => void, duration: number) => void;
 
 export function useSetTimeout(): SetTimeoutType {
-	const timeouts = useRef<number[]>([]);
+	const timeouts = useRef<NodeJS.Timeout[]>([]);
 
 	const wrappedSetTimeout = useCallback<SetTimeoutType>((handler, duration) => {
 		const timeout = setTimeout(handler, duration);
