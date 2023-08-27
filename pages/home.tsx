118eca6dc95405f7b1ebd17ff14c98fa5c0c9fb2
Joseph Stewart <josephlikescode@gmail.com>
2023-08-26 17:20:21 -0700

    Add locationId to home.

--- pages/home.tsx
+++ pages/home.tsx
@@ -17,6 +17,7 @@ export default function Home() {
 		<>
 			<Head />
 			<Location
+				locationId="TheCity"
 				title="Welcome to the Land of Emunah"
 				coverImageUrl="/images/locations/map.png"
 				bodyText="The Land of Emunah is a world where imagination and collaboration flow together. You are invited to embark on a journey of self-discovery and exploration of a world that exists outside of our own. In this land, you will follow a path that will show not just your own personality and consciousness, but also something deeper within you: your soul and creativity. Emunah is a place where you will be able to explore as well as contribute with your own imagination and worlds within your own head. For now, enter and explore what’s set before you in the land of Emunah."
